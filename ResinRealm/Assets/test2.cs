e4a2d8c6b8c696c96a1cff10d2bbdbf65ebb5dc4
affordablecamper <dmb219910rules2@gmail.com>
2018-02-04 13:50:30 -0600

    Adding another test
    2/4/2018

--- ResinRealm/Assets/test2.cs
+++ ResinRealm/Assets/test2.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class test : MonoBehaviour {
+public class test2 : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
