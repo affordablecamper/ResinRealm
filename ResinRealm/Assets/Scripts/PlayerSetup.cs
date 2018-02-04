f88f6490f98d6248a95a8152afd22c6545f1dc5b
affordablecamper <dmb219910rules2@gmail.com>
2018-02-04 16:13:22 -0600

    NetWorking changes
    Added player anim sync
    added sync
    2/4/2018

---
+++ ResinRealm/Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using UnityEngine;
+public class PlayerSetup : NetworkBehaviour {
+
+
+    [SerializeField]
+    Behaviour[] componentsToDisable;
+    [SerializeField]
+    GameObject[] gameObjectComponentsToDisable;
+    void Start () {
+
+        if (!isLocalPlayer) {
+
+            for (int i = 0; i < componentsToDisable.Length; i++) {
+
+
+                componentsToDisable[i].enabled = false;
+
+            }
+
+        }
+
+
+       
+	}
+
+
+    public override void PreStartClient()
+    {
+        GetComponent<NetworkAnimator>().SetParameterAutoSend(0, true);
+        Debug.Log("Anim set");
+    }
+
+}
